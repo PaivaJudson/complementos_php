3b826bff9ae4129dedd7fee1beea9d619fdc68a1
Judson Paiva <judson.paiva@hotmail.com>
2023-07-28 22:54:49 +0100

    dados do user a  serem guardados

--- session.php
+++ session.php
@@ -1,3 +1,13 @@
 <?php 
 
+session_start();
 
+echo "<pre>";
+
+$_SESSION['nome'] = 'Judson Paiva';
+$_SESSION['idade'] = '29';
+
+var_dump($_SESSION);
+
+$nome_completo = $_SESSION['nome'];
+echo 'Nome: '. $nome_completo;
