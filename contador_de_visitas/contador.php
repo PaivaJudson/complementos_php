4bff3698cfcc808c7224cfffe1daa5cdafec7313
Judson Paiva <judson.paiva@hotmail.com>
2023-07-29 15:59:50 +0100

    simples login com session

--- contador_de_visitas/contador.php
+++ contador_de_visitas/contador.php
@@ -1,8 +1,34 @@
 <?php
-
 session_start();
 
-if(!isset($_SESSION['contador'])) 
-{
-    $_SESSION['conatdor'] = 0;
+$expiry_time = 30;
+
+if (!isset($_SESSION['contador'])) {
+    $_SESSION['contador'] = 0;
 }
+
+if(isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $expiry_time)
+{
+    session_unset();
+    session_destroy();
+    header("Location: contador.php");
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contador de Visitas</title>
+</head>
+
+<body>
+    <h1>Contador de Visitas</h1>
+    <p>Você visitou esta página <?php echo $_SESSION['contador']; ?> vezes.</p>
+</body>
+
+</html>
